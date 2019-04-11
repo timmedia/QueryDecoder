fd1cae421daaa6c8728cf31ff419bbdea27f1faf
Tim <contact@tim-media.com>
2019-04-11 21:21:51 +0200

    Update decode.js

--- decode.js
+++ decode.js
@@ -3,7 +3,7 @@ function decode () {
   window.location.search
     .substr(1)
     .split('&')
-    .map(e => e.split('=' ))
+    .map(e => e.split('='))
     .map(e => query[e[0]] = e[1])
   return query
 }
